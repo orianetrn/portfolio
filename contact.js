d55b1a55066cec91a9003a07bdde2da3b380fe95
Oriane Tourneroche <orianetourneroche@MacBook-Pro-2.local>
2022-02-25 19:21:04 +0100

    save1

--- contact.js
+++ contact.js
@@ -17,4 +17,27 @@ window.addEventListener("DOMContentLoaded", (event) => {
        msg.innerHTML = 'Merci ' + prenom.value + ' ' + nom.value + ' pour ton message ! Je te recontacte au plus vite à cette adresse mail : ' + email.value + ' si nécessaire.'; 
    } 
     
+   let i = 0;
+   let svg  = d3.select('body')
+                .append('svg')
+                .attr('width',1920)
+                .attr('height',1080)
+                .on('mousemove',anim);
+
+    function anim(){
+
+        var coord = d3.mouse(this);
+        //console.log(coord);
+                    
+         svg.insert('circle', 'rect')
+        .attr('cx', coord[0])
+        .attr('cy', coord[1])
+        .attr('r', 0)
+        .transition()
+        .duration(1500)
+        .ease(d3.easeCircleOut)
+        .attr('r', 20)
+        .style('opacity', 0)
+        .remove();
+    }
 })
--- portfolio.js
+++ portfolio.js
@@ -1,5 +1,7 @@
 window.addEventListener("DOMContentLoaded", (event) => {
 
+    // animation machine à écrire
+
     const txtAnim = document.querySelector('h2');
 
     new Typewriter(txtAnim, {
@@ -28,6 +30,8 @@ window.addEventListener("DOMContentLoaded", (event) => {
     .pauseFor(1000)
     .start()
     
+    // carrousel
+
     document.body.onload=function(){
         nbr=5;
         p=0;
@@ -70,6 +74,99 @@ window.addEventListener("DOMContentLoaded", (event) => {
             g.style.visibility="visible";
     }
 
+
+
+    // lecteur vidéo
+/*
+let video = document.querySelector('.video');
+let juice = document.querySelector('.orange-juice');
+let btn = document.getElementById('play-pause');
+let muteBtn = document.getElementById('mute');
+let volumeslider = document.getElementById('volumeSlider');
+let orangeBar = document.querySelector('.orange-bar');
+
+function togglePlayPause() {
+
+    if(video.paused){
+        btn.className="pause";
+        video.play();
+    }
+    else {
+        btn.className = "play";
+        video.pause();
+    }
+
+}
+
+btn.onclick = function(){
+    togglePlayPause();
+}
+
+        // barre orange
+
+video.addEventListener('timeupdate', function(){
+
+    let juicePos = video.currentTime / video.duration;
+
+    juice.style.width = juicePos * 100 + '%';
+
+    if(video.ended) {
+        btn.className ="play";
+    }
+
+
+})
+
+// mute la video
+
+
+muteBtn.addEventListener('click', function(){
+
+    if(video.muted){
+        video.muted = false;
+        muteBtn.innerHTML = "Mute";
+    } else {
+        video.muted = true;
+        muteBtn.innerHTML = "Unmute";
+    }
+
+})
+
+// Volume
+
+
+volumeslider.addEventListener('change', function(){
+
+
+    video.volume = volumeslider.value / 100;
+
+})
+
+
+// la barre orange clikable
+
+let rect = orangeBar.getBoundingClientRect();
+console.log(rect);
+
+let largeur = rect.width;
+
+orangeBar.addEventListener('click', function(e){
+
+
+    // la valeur de notre click sur x par rapport a notre element
+
+    let x = e.clientX - rect.left;
     
+    let widthPercent = ((x*100)/largeur);
+    
+    let currentTimeTrue = (widthPercent * 63 ) / 100;
+    
+    // position en secondes
+    video.currentTime = currentTimeTrue;
+    // barre orange qui va la ou on clique
+    juice.style.width = widthPercent + '%';
+
+})
+*/
 })
 
