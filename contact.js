903979caa1d2eba2ae87f0bdbca48fa8c7946bc8
Oriane Tourneroche <orianetourneroche@MacBook-Pro-2.local>
2022-02-27 21:36:04 +0100

    save 2

--- contact.js
+++ contact.js
@@ -11,23 +11,27 @@ window.addEventListener("DOMContentLoaded", (event) => {
        const nom = document.getElementById('nom');
        const prenom = document.getElementById('prenom');
        const email = document.getElementById('email');
+       const message = document.getElementById('message');
 
-       const msg = document.getElementById('msg');     
+       const msg = document.getElementById('msg');   
+       
+       alert ('Le message : ' + message.value + ' a bien été envoyé.');
 
        msg.innerHTML = 'Merci ' + prenom.value + ' ' + nom.value + ' pour ton message ! Je te recontacte au plus vite à cette adresse mail : ' + email.value + ' si nécessaire.'; 
    } 
     
+   // animation sourie
+
    let i = 0;
-   let svg  = d3.select('body')
+   let svg  = d3.select('#animation')
                 .append('svg')
                 .attr('width',1920)
-                .attr('height',1080)
+                .attr('height',200)
                 .on('mousemove',anim);
 
     function anim(){
 
         var coord = d3.mouse(this);
-        //console.log(coord);
                     
          svg.insert('circle', 'rect')
         .attr('cx', coord[0])
--- portfolio.js
+++ portfolio.js
@@ -5,8 +5,8 @@ window.addEventListener("DOMContentLoaded", (event) => {
     const txtAnim = document.querySelector('h2');
 
     new Typewriter(txtAnim, {
-        loop: true,
-        deleteSpeed: 40
+        loop: true ,
+        deleteSpeed: 40,
     })
     .changeDelay(40)
     .typeString ('Viens découvrir mes projets réalisés avec :')
@@ -25,21 +25,66 @@ window.addEventListener("DOMContentLoaded", (event) => {
     .deleteChars(10)
     .typeString('<span style="color: #59d102"> Premiere Pro !</span>')
     .pauseFor(1000)
-    .deleteChars(14)
-    .typeString('<span style="color: #8f00ff"> Wordpress !</span>')
-    .pauseFor(1000)
     .start()
     
-    // carrousel
+    // carrousel rosignol
+
+const imgs = document.getElementById("img");
+const img = document.querySelectorAll ("#img img");
+
+let i=0;
+
+function carrousel(){
+    i++;
+    if(i > img.length -1){
+        i=0;
+    }
+    imgs.style.transform = `translateX(${-i *530}px)`;
+}
+setInterval(carrousel, 2000);
+
+// carrousel halloween
+
+const imgs2 = document.getElementById("img2");
+const img2 = document.querySelectorAll ("#img img");
+
+let j=0;
+
+function carrousel2(){
+    j++;
+    if(j > img2.length -1){
+        j=0;
+    }
+    imgs2.style.transform = `translateX(${-j *420}px)`;
+}
+setInterval(carrousel2, 2000);
+
+// carrousel resto pizza
+
+const imgs3 = document.getElementById("img4");
+const img3 = document.querySelectorAll ("#img img");
+
+let k=0;
+
+function carrousel3(){
+    k++;
+    if(k > img3.length -1){
+        k=0;
+    }
+    imgs3.style.transform = `translateX(${-k *600}px)`;
+}
+setInterval(carrousel3, 2000);
+
+    // carrousel portfolio
 
     document.body.onload=function(){
-        nbr=5;
+        nbr=6;
         p=0;
         container=document.getElementById("container");
         g=document.getElementById("g");
         d=document.getElementById("d");
         container.style.width = (800*nbr)+"px";
-        for(i=1; i<=nbr; i++){
+        for(i=0; i<=nbr; i++){
             div=document.createElement("div");
             div.className = "photo";
             div.style.backgroundImage = "url(img"+i+".png)";
@@ -50,14 +95,6 @@ window.addEventListener("DOMContentLoaded", (event) => {
     g.onclick=function(){
         if(p<0)
         p++;
-        container.style.transform="translate("+p*800+"px)";
-        container.style.transition="all 1s ease";
-        afficherMasquer();
-    }
-
-    d.onclick=function(){
-        if(p>-nbr+1)
-        p--;
         container.style.transform="translate("+p*800+"px)";
         container.style.transition="all 1s ease";
         afficherMasquer();
@@ -74,99 +111,13 @@ window.addEventListener("DOMContentLoaded", (event) => {
             g.style.visibility="visible";
     }
 
-
-
-    // lecteur vidéo
-/*
-let video = document.querySelector('.video');
-let juice = document.querySelector('.orange-juice');
-let btn = document.getElementById('play-pause');
-let muteBtn = document.getElementById('mute');
-let volumeslider = document.getElementById('volumeSlider');
-let orangeBar = document.querySelector('.orange-bar');
-
-function togglePlayPause() {
-
-    if(video.paused){
-        btn.className="pause";
-        video.play();
-    }
-    else {
-        btn.className = "play";
-        video.pause();
+    d.onclick=function(){
+        if(p>-nbr+1)
+        p--;
+        container.style.transform="translate("+p*800+"px)";
+        container.style.transition="all 1s ease";
+        afficherMasquer();
     }
 
-}
-
-btn.onclick = function(){
-    togglePlayPause();
-}
-
-        // barre orange
-
-video.addEventListener('timeupdate', function(){
-
-    let juicePos = video.currentTime / video.duration;
-
-    juice.style.width = juicePos * 100 + '%';
-
-    if(video.ended) {
-        btn.className ="play";
-    }
-
-
-})
-
-// mute la video
-
-
-muteBtn.addEventListener('click', function(){
-
-    if(video.muted){
-        video.muted = false;
-        muteBtn.innerHTML = "Mute";
-    } else {
-        video.muted = true;
-        muteBtn.innerHTML = "Unmute";
-    }
-
-})
-
-// Volume
-
-
-volumeslider.addEventListener('change', function(){
-
-
-    video.volume = volumeslider.value / 100;
-
-})
-
-
-// la barre orange clikable
-
-let rect = orangeBar.getBoundingClientRect();
-console.log(rect);
-
-let largeur = rect.width;
-
-orangeBar.addEventListener('click', function(e){
-
-
-    // la valeur de notre click sur x par rapport a notre element
-
-    let x = e.clientX - rect.left;
-    
-    let widthPercent = ((x*100)/largeur);
-    
-    let currentTimeTrue = (widthPercent * 63 ) / 100;
-    
-    // position en secondes
-    video.currentTime = currentTimeTrue;
-    // barre orange qui va la ou on clique
-    juice.style.width = widthPercent + '%';
-
-})
-*/
 })
 
